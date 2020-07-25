74484577556b54051c768765a138162ab647bd1a
Shiyuan Huang <19438967+Shiyuan-Huang-23@users.noreply.github.com>
2020-07-24 23:43:08 -0400

    Build structure of main section (#2)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="body">
-      <h1>Shiyuan Huang</h1>
+    <div className="main">
+      <h1 id="main-name">Shiyuan Huang</h1>
+      <h2 id="main-descrip">CS @ Cornell</h2>
+      <a
+        href="https://github.com/Shiyuan-Huang-23"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="main-icon"
+      >
+        <ion-icon name="logo-github"></ion-icon>
+      </a>
     </div>
   );
 }
